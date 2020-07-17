661d73d085ee43a5baac0ae65fa46194a902d5c8
Swaroop Ghosh <swaroop.me@gmail.com>
2020-07-17 19:37:18 +0530

    Added initial files

---
+++ index.js
@@ -0,0 +1,16 @@
+
+class Index {
+
+    
+    constructor(t) {
+        console.log('Test');
+
+        this.t = t;
+    }
+
+    getConvertedValue() {
+        return "Tst";
+    }
+}
+module.exports = Index;
+let i = new Index('as');
